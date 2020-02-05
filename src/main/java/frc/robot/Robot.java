43e3cb8ddeaf086221958e70595dc5c2ab1c2920
89apt89 <89apt89@protonmail.ch>
2020-02-05 08:09:13 -0500

    add color sensor code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,20 +1,29 @@
 package frc.robot;
 
+import com.revrobotics.ColorSensorV3;
+
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 import frc.robot.commands.DriveCommand;
+import frc.robot.controllers.Controller;
 import frc.robot.controllers.PingController;
 import frc.robot.subsystems.CSMDriveTrain;
 import frc.robot.subsystems.ColorSensorSubsystem;
 import frc.robot.subsystems.DriveTrainSubsystem;
+import frc.robot.utils.Config;
+import frc.robot.utils.I2CCOM;
 
 public class Robot extends TimedRobot {
   public static DriveTrainSubsystem m_drivetrainsubsystem = new CSMDriveTrain(); // CAN Spark MAX motor
   public static ColorSensorSubsystem m_colorsensorsubsystem = new ColorSensorSubsystem();
+  public static ColorSensorV3 m_colorsensor = m_colorsensorsubsystem.colorSensor;
+
+  I2CCOM arduinoI2C;
 
   public PingController pingController;
 
@@ -68,6 +77,26 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
     Scheduler.getInstance().run();
     driveCommand.start();
+
+    
+    Controller controller = Config.getController("controls.main");
+    if (controller.getButton(1)) {
+      arduinoI2C.sendData(1, 1);
+    }
+    if (controller.getButton(2)) {
+      arduinoI2C.sendData(1, 0);
+
+      Color detectedColor = m_colorsensor.getColor();
+      double IR = m_colorsensor.getIR();
+      SmartDashboard.putNumber("Red", detectedColor.red);
+      SmartDashboard.putNumber("Green", detectedColor.green);
+      SmartDashboard.putNumber("Blue", detectedColor.blue);
+      SmartDashboard.putNumber("IR", IR);
+
+      int proximity = m_colorsensor.getProximity();
+
+      SmartDashboard.putNumber("Proximity", proximity);
+    } 
   }
 
   @Override
