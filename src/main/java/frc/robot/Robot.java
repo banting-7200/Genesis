9a5aefb73fde831cbd27b50e9c73471742d223cd
mat4539 <mborg0020@gmail.com>
2020-02-05 07:56:26 -0500

    code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,19 +10,6 @@ import frc.robot.commands.DriveCommand;
 import frc.robot.controllers.PingController;
 import frc.robot.subsystems.CSMDriveTrain;
 import frc.robot.subsystems.DriveTrainSubsystem;
-<<<<<<< HEAD
-
-=======
-import frc.robot.subsystems.PneumaticsSubsystem;
-import frc.robot.utils.I2CCOM;
-import frc.robot.utils.Config;
-import edu.wpi.first.wpilibj.CameraServer;
-import com.revrobotics.ColorSensorV3;
-import edu.wpi.first.wpilibj.I2C.Port;
-import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.util.Color;
->>>>>>> Colorfbruicwc
 
 public class Robot extends TimedRobot {
   public static DriveTrainSubsystem m_drivetrainsubsystem = new CSMDriveTrain(); // CAN Spark MAX motor
@@ -38,12 +25,6 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", new DriveCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
-<<<<<<< HEAD
-=======
-    arduinoI2C = new I2CCOM(1);
-    rPiI2C = new I2CCOM(2);
-    
->>>>>>> Colorfbruicwc
   }
 
   @Override
