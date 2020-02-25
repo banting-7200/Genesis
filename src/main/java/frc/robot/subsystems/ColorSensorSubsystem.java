2e6d342d4afdbe4a4071890bdd2eb17523fcba1d
mat4539 <mborg0020@gmail.com>
2020-02-25 17:16:28 -0500

    colour sensor

--- src/main/java/frc/robot/subsystems/ColorSensorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ColorSensorSubsystem.java
@@ -1,15 +1,10 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.ColorSensorV3;
-import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import com.revrobotics.ColorMatch;
 import edu.wpi.first.wpilibj.util.Color;
 
 public class ColorSensorSubsystem extends Subsystem{
-    
-  public final I2C.Port i2cPort = I2C.Port.kOnboard; // Initializes the I2C port on the RoboRIO for the Color Sensor - D
-  public final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort); // Initializes the color sensor for use
   public final ColorMatch m_colorMatcher = new ColorMatch(); // Creates an object that lets us match colours
 
   public final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429); // This line establishes what should be a like-blue colour
@@ -18,14 +13,13 @@ public class ColorSensorSubsystem extends Subsystem{
   public final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113); // This line establishes what should be a like-yellow colour
 
 
-  public ColorSensorSubsystem() {
+  public void initialise() {
     m_colorMatcher.addColorMatch(kBlueTarget);
     m_colorMatcher.addColorMatch(kGreenTarget);
     m_colorMatcher.addColorMatch(kRedTarget);
     m_colorMatcher.addColorMatch(kYellowTarget);
   }
-
-
+  
   @Override
   public void initDefaultCommand() {
 
