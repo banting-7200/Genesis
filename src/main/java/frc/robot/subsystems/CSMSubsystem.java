e5d89aaebbbf5bbede4a10fbd7e624a685ccb416
89apt89 <89apt89@protonmail.ch>
2020-02-25 16:38:12 -0500

    format CSMSubsystem

--- src/main/java/frc/robot/subsystems/CSMSubsystem.java
+++ src/main/java/frc/robot/subsystems/CSMSubsystem.java
@@ -1,16 +1,12 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import edu.wpi.first.wpilibj.SpeedController;
-import frc.robot.controllers.Controller;
-import frc.robot.utils.Config;
 import com.revrobotics.CANEncoder;
-
 
 import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
- 
+
 public class CSMSubsystem extends Subsystem {
 
     public CANSparkMax NEWCSM;
@@ -21,52 +17,51 @@ public class CSMSubsystem extends Subsystem {
         this.NEWCSM.set(0);// initially sets the motor to stop
     }
 
-
-    public void start(int speed){
+    public void start(int speed) {
         this.NEWCSM.set(speed);
     }
 
-    public void stop(){
+    public void stop() {
         this.NEWCSM.set(0);
     }
 
-    public void encoderup(int canadress, int rotation){
+    public void encoderup(int canadress, int rotation) {
         this.m_encoder = new CANEncoder(NEWCSM);
         this.NEWCSM = new CANSparkMax(canadress, MotorType.kBrushless);
         System.out.println(m_encoder.getPosition());
         double liftspeed = (m_encoder.getPosition() - rotation) / -20;// this sets the lift speed and slows the motor
-																			// down as it gets nearer to its stopping
-																			// point
+                                                                      // down as it gets nearer to its stopping
+                                                                      // point
 
-				if (liftspeed >= 1.0) {
-					liftspeed = 1.0;// the motor cannot run faster than 1.0, so if a faster run is requested, this
-									// will constrain the value
-				}
-				if (liftspeed <= -0.6) {
-					liftspeed = 0.6;
-				}
+        if (liftspeed >= 1.0) {
+            liftspeed = 1.0;// the motor cannot run faster than 1.0, so if a faster run is requested, this
+                            // will constrain the value
+        }
+        if (liftspeed <= -0.6) {
+            liftspeed = 0.6;
+        }
 
-				this.NEWCSM.set(liftspeed);// run the motor
+        this.NEWCSM.set(liftspeed);// run the motor
     }
 
-    public void encoderdown(int canadress){
+    public void encoderdown(int canadress) {
         this.m_encoder = new CANEncoder(NEWCSM);
         this.NEWCSM = new CANSparkMax(canadress, MotorType.kBrushless);
         System.out.println(m_encoder.getPosition());
         double liftspeed = (m_encoder.getPosition()) / -20;
 
-				if (liftspeed <= -0.6) {
-					liftspeed = -0.6;
-				}
+        if (liftspeed <= -0.6) {
+            liftspeed = -0.6;
+        }
 
-				this.NEWCSM.set(liftspeed);
-       
+        this.NEWCSM.set(liftspeed);
+
     }
 
     @Override
     protected void initDefaultCommand() {
         // TODO Auto-generated method stub
-        // fucking usless garbage 
+        // fucking usless garbage
 
     }
 
