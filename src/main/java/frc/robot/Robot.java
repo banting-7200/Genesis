4334b51975f2f106752ff69f536cfdd13be91e3d
89apt89 <89apt89@protonmail.ch>
2020-02-27 14:04:37 -0500

    shift spark code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,6 +13,7 @@ import frc.robot.commands.LiftCommand;
 import frc.robot.controllers.Controller;
 import frc.robot.controllers.PingController;
 import frc.robot.subsystems.PneumaticsSubsystem;
+import frc.robot.subsystems.SparkSubsystem;
 import frc.robot.subsystems.LimitSubsystem;
 import frc.robot.subsystems.LiftSubsystem;
 import frc.robot.subsystems.CSMDriveTrain;
@@ -25,7 +26,8 @@ public class Robot extends TimedRobot {
   public static DriveTrainSubsystem m_drivetrainsubsystem = new CSMDriveTrain(); // CAN Spark MAX motor
   public static ColorSensorSubsystem m_colorsensorsubsystem = new ColorSensorSubsystem();
   public static LiftSubsystem m_liftsubsystem = new LiftSubsystem();
-  public static LimitSubsystem limitSwitch =  new LimitSubsystem(1);
+  public static SparkSubsystem shiftSpark = new SparkSubsystem(7);
+  public static LimitSubsystem limitSwitch = new LimitSubsystem(1);
 
   I2CCOM arduinoI2C;
 
@@ -40,7 +42,6 @@ public class Robot extends TimedRobot {
   Command intakeCommand = new IntakeCommand();
   Command intakeLiftCommand = new IntakeLiftCommand();
   Command m_autonomousCommand;
-   
 
   SendableChooser<Command> m_chooser = new SendableChooser<>();
 
@@ -48,14 +49,14 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", new DriveCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
-    this.colorwheelpiston = new PneumaticsSubsystem(1, 1);//setting the can Adress of the PCM and the port on PCM
-    
+    this.colorwheelpiston = new PneumaticsSubsystem(1, 1);// setting the can Adress of the PCM and the port on PCM
+
   }
 
   @Override
   public void robotPeriodic() {
-  //this.findColor.ColorSencorSubsystem();
-  //System.out.println(findColor);
+    // this.findColor.ColorSencorSubsystem();
+    // System.out.println(findColor);
   }
 
   @Override
@@ -83,7 +84,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    //CameraServer.getInstance().startAutomaticCapture();
+    // CameraServer.getInstance().startAutomaticCapture();
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
@@ -94,40 +95,48 @@ public class Robot extends TimedRobot {
     Controller controller = Config.getController("controls.main");
     Scheduler.getInstance().run();
     driveCommand.start();
-    //liftCommand.start();
+    // liftCommand.start();
     colorCommand.start();
     intakeCommand.start();
     intakeLiftCommand.start();
-    
+
     boolean shootColorWheel = controller.getButton(6);
     boolean retractColorWheel = controller.getButton(7);
-//*****************Pneumatics*******************/
-    if(shootColorWheel){
+    // *****************Pneumatics*******************/
+    if (shootColorWheel) {
       this.colorwheelpiston.ToggleSolenoid(true);
       m_drivetrainsubsystem.setSpeed(0.5);
-    } 
-    if (retractColorWheel){
+    }
+    if (retractColorWheel) {
       this.colorwheelpiston.ToggleSolenoid(false);
       m_drivetrainsubsystem.setSpeed(1);
     }
-//***************Pneumatics end*****************/
-//**************Limit Switch Code****************/
-    //if (!limitSwitch.getLimit()){
-    //  System.out.println("Bruh switch bruh switch");
-    //  driveCommand.cancel();
-    //}else{
-    //}
-//************Limit Switch Code END***************/
+    // ***************Pneumatics end*****************/
+    // **************Limit Switch Code****************/
+    // if (!limitSwitch.getLimit()){
+    // System.out.println("Bruh switch bruh switch");
+    // driveCommand.cancel();
+    // }else{
+    // }
+    // ************Limit Switch Code END***************/
 
+    /*
+     * if (controller.getButton(1)) { arduinoI2C.sendData(1, 1); } if
+     * (controller.getButton(2)) { arduinoI2C.sendData(1, 0);
+     * 
+     * }
+     */
 
-    /*if (controller.getButton(1)) {
-      arduinoI2C.sendData(1, 1);
+    if (controller.getButton(10)) {
+      System.out.println("Button 10 be like");
+      this.shiftSpark.start(1);
+    } else if (controller.getButton(9)) {
+
+      System.out.println("Button 9 be like");
+      this.shiftSpark.start(-1);
+    } else {
+      this.shiftSpark.stop();
     }
-    if (controller.getButton(2)) {
-      arduinoI2C.sendData(1, 0);
-
-    }*/
-    
   }
 
   @Override
